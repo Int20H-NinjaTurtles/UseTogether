cae80d2c4e6f762dc1f5267dfd06fa18f02b8633
rodionkushch <rodion.kushch@uptech.team>
2021-02-20 19:06:09 +0200

    add test App Actions

--- app/src/main/java/com/ninjaturtles/usetogether/MainActivity.kt
+++ app/src/main/java/com/ninjaturtles/usetogether/MainActivity.kt
@@ -7,17 +7,9 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.navigation.findNavController
 import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    object DeepLink {
-        val REQUEST_RIDE = "actions.intent.REQUEST_RIDE"
-
-        object Params {
-            val START_POINT = "start_point"
-            val END_POINT = "end_point"
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,26 +19,17 @@ class MainActivity : AppCompatActivity() {
 
 
     private fun handleDeepLink(data: Uri?) {
-        when (data?.path) {
-            DeepLink.REQUEST_RIDE -> {
-                // Get the parameter defined as "exerciseType" and add it to the fragment arguments
-                val startPoint = data.getQueryParameter(DeepLink.Params.START_POINT).orEmpty()
-                val endPoint = data.getQueryParameter(DeepLink.Params.END_POINT).orEmpty()
-            }
-            else -> {
-                showDefaultView()
-            }
-        }
+        info.text = data.toString()
+//        val startPoint = data?.getQueryParameter(DeepLink.Params.START_POINT).orEmpty()
+//        val endPoint = data?.getQueryParameter(DeepLink.Params.END_POINT).orEmpty()
     }
 
     private fun Intent.handleIntent() {
         when (action) {
             Intent.ACTION_VIEW -> handleDeepLink(data)
-            else -> showDefaultView()
-        }
-    }
+            else -> {
 
-    private fun showDefaultView() {
-        Toast.makeText(this, "Unsupported operation", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
