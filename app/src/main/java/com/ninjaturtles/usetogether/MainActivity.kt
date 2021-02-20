311186e709a7b4b3012eb498d23160ade5e8bf36
KoliaHuk <m.hyk@polytech.software>
2021-02-20 11:24:28 +0200

    setup jetpack navigation

--- app/src/main/java/com/ninjaturtles/usetogether/MainActivity.kt
+++ app/src/main/java/com/ninjaturtles/usetogether/MainActivity.kt
@@ -2,6 +2,8 @@ package com.ninjaturtles.usetogether
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
